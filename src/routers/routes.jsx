abe93ac23a81fdf2af14f76d1e9983ed9de15977
Erik Hernandez <erikestuardo_161@hotmail.com>
2025-05-27 23:28:30 -0600

    The router for the sidebar was configured to appear on all pages, the BrowserRouter was modified

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,11 +1,11 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {Routes, Route} from "react-router-dom"
 import { Home } from "../index"
 export function MyRoutes(){
     return (
-        <BrowserRouter>
+
             <Routes>
                 <Route path="/" element={<Home/>}/>
             </Routes>
-        </BrowserRouter>
+
     )
 }
