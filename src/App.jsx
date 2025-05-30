9ffdd35537597c480092f8c3159cb9eace2f3c6f
Erik Hernandez <erikestuardo_161@hotmail.com>
2025-05-29 23:52:52 -0600

    The routes were configured and the styles for the dark and light themes were consumed | Part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {AuthContextProvider, MyRoutes} from "./index"
+import styled from "styled-components";
+import {AuthContextProvider, MyRoutes, Light, Dark} from "./index"
+import { createContext, useState } from 'react'
+export const ThemeContext = createContext(null);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeuse, setTheme] = useState("dark");
+  const theme = themeuse==="light" ? "light" : "dark";
+  const themeStyle = theme==="light" ? Light : Dark;
 
   return (
     <>
-      <AuthContextProvider>
-        <MyRoutes/>
-      </AuthContextProvider>
+      <ThemeContext.Provider value={{theme, setTheme}}>
+        <ThemeProvider theme={themeStyle}>
+          <AuthContextProvider>
+            <MyRoutes/>
+          </AuthContextProvider>
+        </ThemeProvider>
+      </ThemeContext.Provider>
     </>
   )
 }
+
+const Container = styled.div`
+  display:grid;
+  grid-template-columns:1fr;
+  background-color:${(props)=>props.theme.bgtotal};
+  
+`;
 
 export default App
