a4033cdd0c311063880f6fee70e9034762b7659f
Erik Hernandez <erikestuardo_161@hotmail.com>
2025-08-15 01:31:22 -0600

    Added a toggle in the mobile version

--- src/components/organismos/Menuhambur.jsx
+++ src/components/organismos/Menuhambur.jsx
@@ -4,7 +4,12 @@ export function MenuHambur() {
 <NavBar>
     <section>
         <HamburguerMenu>
-          
+          <input id="checkbox2" type="checkbox"/>
+          <label class="toggle toggle2" for="checkbox2">
+            <div id="bar4" class="bars"></div>
+            <div id="bar5" class="bars"></div>
+            <div id="bar6" class="bars"></div>
+          </label>
         </HamburguerMenu>
     </section>
 
@@ -23,10 +28,68 @@ const NavBar =styled.nav`
 `
 
 const HamburguerMenu =styled.span`
+
+position:fixed;
+top:2rem;
+z-index:100;
+
+#checkbox2 {
+  display: none;
+}
+
+.toggle2 {
+  position: relative;
+  width: 25px;
+  height: 25px;
+  cursor: pointer;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   align-items: center;
-  width: 20px;
-  height: 20px;
+  justify-content: center;
+  gap: 12px;
+  transition-duration: .5s;
+}
+
+.bars {
+  width: 100%;
+  height: 6px;
+  background-color: rgb(92, 176, 255);
+  border-radius: 6px;
+}
+
+#bar5 {
+  transition-duration: .8s;
+}
+
+#bar4,#bar6 {
+  width: 80%;
+}
+
+#checkbox2:checked + .toggle2 .bars {
+  position: absolute;
+  transition-duration: .5s;
+}
+
+#checkbox2:checked + .toggle2 #bar5 {
+  transform: scaleX(0);
+  transition-duration: .5s;
+}
+
+#checkbox2:checked + .toggle2 #bar4 {
+  width: 100%;
+  transform: rotate(45deg);
+  transition-duration: .5s;
+}
+
+#checkbox2:checked + .toggle2 #bar6 {
+  width: 100%;
+  transform: rotate(-45deg);
+  transition-duration: .5s;
+}
+
+#checkbox2:checked + .toggle2 {
+  transition-duration: .5s;
+  transform: rotate(180deg);
+}
 `
+
